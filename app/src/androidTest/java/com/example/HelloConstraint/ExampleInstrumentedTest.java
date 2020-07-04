6fabc0dfa8d4e57f3e0ee3e17e5df58aa04bc9e7
ling63 <liewsl-wm19@student.tarc.edu.my>
2020-07-04 18:19:26 +0800

    commit by liew siew ling 29/6/2020

--- app/src/androidTest/java/com/example/HelloConstraint/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/HelloConstraint/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.practical_2_part_a;
+package com.example.HelloConstraint;
 
 import android.content.Context;
 
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.practical_2_part_a", appContext.getPackageName());
+        assertEquals("com.example.HelloConstraint", appContext.getPackageName());
     }
 }
---
+++ app/src/main/java/com/example/HelloConstraint/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.HelloConstraint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+    private Button mResetButton;
+    private Button mCountButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        mResetButton = findViewById(R.id.button_zero);
+        mCountButton = findViewById(R.id.button_count);
+    }
+
+    public void showToast(View view){
+        Toast toast = Toast.makeText(this, R.string.toast_message,Toast.LENGTH_SHORT);
+
+        toast.show();
+    }
+
+    public void countUP(View view){
+        ++mCount;
+        if(mShowCount != null) {
+            mShowCount.setText(Integer.toString(mCount));
+        }
+        mResetButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        mResetButton.setClickable(true);
+
+        switch (mCount % 2) {
+            case 0:
+                view.setBackgroundColor(Color.MAGENTA);
+                break;
+            case 1:
+                view.setBackgroundColor(Color.GREEN);
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void resetZero(View view){
+        mCount =0;
+        mShowCount.setText(String.valueOf(mCount));
+
+        view.setClickable(false);
+        view.setBackgroundColor(Color.GRAY);
+        mCountButton.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+    }
+
+}
